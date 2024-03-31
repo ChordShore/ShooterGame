82acfb0d837a592c8be79462dc2d65927e6502e4
Chord Shore <socials@chordshore.com>
2024-03-31 02:54:19 -0700

    Fixed Ambience
    Tweaked Holograph Diffusion & Obstruction until it worked
    Set all ambience attenuations tighter
    Set all ambience volumes quieter
    Reset Room Portals & volume dimensions
    
    If at first you don't succeed, tweak settings for 3 hours until you do.

--- Plugins/Wwise/Source/WwiseSoundEngine/Public/Generated/AkiOSPlugins.h
+++ Plugins/Wwise/Source/WwiseSoundEngine/Public/Generated/AkiOSPlugins.h
@@ -17,7 +17,6 @@ Copyright (c) 2023 Audiokinetic Inc.
 
 #if PLATFORM_IOS
 #include <AK/Plugin/AkToneSourceFactory.h>
-#include <AK/Plugin/AkParametricEQFXFactory.h>
 #include <AK/Plugin/AkDelayFXFactory.h>
 #include <AK/Plugin/AkPeakLimiterFXFactory.h>
 #include <AK/Plugin/AkRoomVerbFXFactory.h>
